5e1bbbd9187fc351eb8b99b51f6036edc917eb54
Lucas Sweeney <lsweeney0124@gmail.com>
2022-06-07 22:39:10 -0500

    commit tree 220855a6d5f98c6de8a437c57b4c3fba23523043

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
 import React from "react";
 import blogData from "../data/blog";
-
+import Header from "../components/Header"
 console.log(blogData);
 
-function App() {
+function Header (props) {
+  return (
+    <header>
+      <h1> { blogData.name } </h1>
+    </header>
+  )
+}
+
+function App () {
   return (
     <div className="App">
       You're on your own from here! Follow the deliverables; test things out in
       the browser as you write your code; and good luck!
+      <Header />
     </div>
   );
 }
